6409454ce0993e45ccc6b6bbbb987ab99c6b2222
Logan <thecrazykid2121@gmail.com>
2024-02-25 12:49:56 -0500

    Cross-platform conditionals

--- kolf.cpp
+++ kolf.cpp
@@ -26,11 +26,21 @@
 #include "scoreboard.h"
 
 #include <iostream>
+#include <string>
 #include <cstring>
+#ifdef _WIN32
 #include <winsock2.h>
 #include <ws2tcpip.h>
-
 #pragma comment(lib, "ws2_32.lib")
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#define SOCKET int
+#define INVALID_SOCKET -1
+#define closesocket close
+#endif
 
 #include <KActionCollection>
 #include <KIO/CopyJob>
@@ -663,48 +673,62 @@ void KolfWindow::openUrl(const QUrl &url)
 
 
 void sendJsonToServer(const char* json_data) {
+	// if windows
+#ifdef _WIN32
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         std::cerr << "WSAStartup failed\n";
         return;
     }
+#endif
+
 
     SOCKET client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (client_socket == INVALID_SOCKET) {
-        std::cerr << "Error creating socket: " << WSAGetLastError() << std::endl;
+        std::cerr << "Error creating socket\n";
+#ifdef _WIN32
+        std::cerr << "Error code: " << WSAGetLastError() << std::endl;
         WSACleanup();
+#endif
         return;
     }
+
 
     sockaddr_in server_address{};
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORT);
+    inet_pton(AF_INET, SERVER_IP, &server_address.sin_addr);
 
-    if (inet_pton(AF_INET, SERVER_IP, &(server_address.sin_addr)) <= 0) {
-        std::cerr << "Invalid address or address not supported\n";
-        closesocket(client_socket);
-        WSACleanup();
-        return;
-    }
 
     if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == SOCKET_ERROR) {
-        std::cerr << "Error connecting to the server: " << WSAGetLastError() << std::endl;
+        std::cerr << "Error connecting to the server\n";
+#ifdef _WIN32
+        std::cerr << "Error code: " << WSAGetLastError() << std::endl;
         closesocket(client_socket);
         WSACleanup();
+#else
+        closesocket(client_socket);
+#endif
         return;
     }
 
-    // Create an HTTP request with the JSON data
+    
     std::string http_request = "POST /turn HTTP/1.1\r\n"
                                "Host: " + std::string(SERVER_IP) + "\r\n"
                                "Content-Type: application/json\r\n"
                                "Content-Length: " + std::to_string(strlen(json_data)) + "\r\n"
                                "\r\n" + std::string(json_data);
 
+
     send(client_socket, http_request.c_str(), http_request.size(), 0);
 
+
     closesocket(client_socket);
+
+
+#ifdef _WIN32
     WSACleanup();
+#endif
 }
 
 void KolfWindow::newPlayersTurn(Player *player)
